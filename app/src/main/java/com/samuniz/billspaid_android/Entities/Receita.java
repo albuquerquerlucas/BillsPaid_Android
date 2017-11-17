b5a0a251ab285a7078649b83cb83f0bfb7bfd8af
samukamuniz <samuka.muniz@gmail.com>
2017-11-16 21:44:33 -0300

    Criando a entidade Receita

---
+++ app/src/main/java/com/samuniz/billspaid_android/Entities/Receita.java
@@ -0,0 +1,42 @@
+package com.samuniz.billspaid_android.Entities;
+
+import java.io.Serializable;
+
+public class Receita implements Serializable{
+    private String id;
+    private String descricao;
+    private String valor;
+
+    public Receita (){
+    }
+
+    public Receita(String id, String descricao, String valor) {
+        this.id = id;
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+}
