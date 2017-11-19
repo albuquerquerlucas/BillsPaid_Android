3e31b82b0a4e5f7767c1b848bbdf7637be2a34b0
samukamuniz <samuka.muniz@gmail.com>
2017-11-19 19:56:16 -0300

    Add atribuindo campos

--- app/src/main/java/com/samuniz/billspaid_android/Entities/Despesa.java
+++ app/src/main/java/com/samuniz/billspaid_android/Entities/Despesa.java
@@ -6,6 +6,10 @@ public class Despesa implements Serializable{
     private String id;
     private String descricao;
     private String valor;
+    private String dataD;
+    private String conta;
+    private String categoria;
+    private String pago;
 
     public Despesa (){
     }
@@ -15,6 +19,16 @@ public class Despesa implements Serializable{
         this.descricao = descricao;
         this.valor = valor;
     }
+
+    /*public Despesa(String id, String descricao, String valor, String dataD, String conta, String categoria, String pago) {
+        this.id = id;
+        this.descricao = descricao;
+        this.valor = valor;
+        this.dataD = dataD;
+        this.conta = conta;
+        this.categoria = categoria;
+        this.pago = pago;
+    }*/
 
     public String getId() {
         return id;
@@ -38,5 +52,37 @@ public class Despesa implements Serializable{
 
     public void setValor(String valor) {
         this.valor = valor;
+    }
+
+    public String getDataD() {
+        return dataD;
+    }
+
+    public void setDataD(String dataD) {
+        this.dataD = dataD;
+    }
+
+    public String getConta() {
+        return conta;
+    }
+
+    public void setConta(String conta) {
+        this.conta = conta;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getPago() {
+        return pago;
+    }
+
+    public void setPago(String pago) {
+        this.pago = pago;
     }
 }
